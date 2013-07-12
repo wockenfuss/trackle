67df103857077f72c1da927b9f74499a375b46a5
Brent Coughenour <wockenfuss@brentcoughenour.com>
2013-07-12 10:07:08 -0700

    added show action to users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,6 +5,10 @@ class UsersController < ApplicationController
 		@user = User.new
 	end
 
+	def show 
+		@user = User.find(params[:id])
+	end
+
 	def create
 		@user = User.create(params[:user])
 		if @user.save
