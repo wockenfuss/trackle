fefdab3187ced97ef37386c0f61f4e5f8a4791b0
Brent Coughenour <wockenfuss@brentcoughenour.com>
2013-07-12 14:55:20 -0700

    user color only updates for current assignment

--- app/controllers/assignments_controller.rb
+++ app/controllers/assignments_controller.rb
@@ -1,10 +1,10 @@
 class AssignmentsController < ApplicationController
 	respond_to :js, :html, :json
 	def create
-		@assignment = Assignment.create(params[:assignment])
+		@assignment = Assignment.new(params[:assignment])
+		@color = @assignment.user.current_color
 		if @assignment.save
-			respond_with(@assignment)
-	
+			respond_with(@assignment, @color)
 		else
 			respond_with( {:error => "error"}, :location => nil)
 		end
