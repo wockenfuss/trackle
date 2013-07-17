cd1f0d4030a62b2fc150f2fd9bf3da09957705fa
Brent Coughenour <wockenfuss@brentcoughenour.com>
2013-07-17 12:05:54 -0700

    added status display to user badge

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,14 @@
 module UsersHelper
+
+	def status_bar(user)
+		if user.current_assignment != "none"
+			if user.current_assignment.status == "in progress"
+				# return "Started at #{user.current_assignment.started_at}"
+				return "Started at #{user.current_assignment.started_at.localtime.strftime("%-m %d %Y, %l:%M %p")}"
+			else
+				return user.current_assignment.status
+			end
+		end
+	end
+
 end
