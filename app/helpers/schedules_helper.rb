d0defcf8b15e961a45dc637fb6a4f5ae1cc9890e
Brent Coughenour <wockenfuss@brentcoughenour.com>
2013-07-17 19:15:44 -0700

    changed styling for task progress bar

--- app/helpers/schedules_helper.rb
+++ app/helpers/schedules_helper.rb
@@ -5,4 +5,7 @@ module SchedulesHelper
 		completed_assignments = total_assignments.where('completed_at is NOT NULL')
 		return "#{completed_assignments.count}/#{total_assignments.count}"
 	end
+	def parse_time(datetime)
+		datetime.strftime("%-m/%d/%Y") if datetime
+	end
 end
